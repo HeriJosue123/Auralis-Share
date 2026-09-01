e7484b3d2f4ae3211cd12d4537948f6c33380bd1
Heriberto Josue Echeverria Cardona <ec1149032024@unab.edu.sv>
2026-08-31 22:07:52 -0600

    chore: reset repository for Auralis Share

--- Estructural/AdapterInterface.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Codigo_Limpio.Estructural
-{
-    public interface AdapterInterface
-    {
-        // Esta es la línea que te falta:
-        string ProcesarDatos(string dato);
-    }
-}
